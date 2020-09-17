85dfe5e99a658f69077b14a96363aeaf82d1211e
june <junearagoncillo>
2020-09-17 15:39:04 +0800

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('./storage/icon/favicon.ico')}}" />
     {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> --}}
 
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
